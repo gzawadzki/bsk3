2d68a2193670c40324defc6cb02a0aa3105b18cc
Krzysiek <krzysiek.kondracikowski@gmail.com>
2020-05-12 18:05:14 +0200

    Dodanie metody make permutation

--- src/sample/desAlgorithm.java
+++ src/sample/desAlgorithm.java
@@ -1,4 +1,23 @@
 package sample;
 
 public class desAlgorithm {
+
+
+
+    /**
+     * Metoda która permutuje bloki bitów zgodnie z podanym rozmiarem i używa tabel z klasy desTables, które są niezbędne do algorytmu des
+     * @param block -> blok do permutacji
+     * @param permArray -> tabela z desTables
+     * @param finalSize -> rozmiar który chcemy osiągnąć np. 48 bitów
+     * @return
+     */
+    private int[] makePermutation(int[] block, int[] permArray, int finalSize) {
+        int[] newBlock = new int[finalSize];
+        for (int i = 0; i < finalSize; i++) {
+            int index = permArray[i] - 1;
+            newBlock[i] = block[index];
+        }
+        return newBlock;
+
+    }
 }
